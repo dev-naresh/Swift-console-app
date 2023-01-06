f4981ebec1bb2812d1fbcd76e0396a255faa4a98
naresh-pt6259 <nareshkumaroviliraja2002@gmail.com>
2023-01-06 12:01:36 +0530

    main commit

--- ProjectManager/ProjectManager/DBManager.swift
+++ ProjectManager/ProjectManager/DBManager.swift
@@ -6,3 +6,31 @@
 //
 
 import Foundation
+import SQLite3
+
+class DBManager {
+    var db: OpaquePointer? = nil
+    
+    init() {
+        openDatabase()
+    }
+    
+    func openDatabase(){
+        //Database path generation
+        let dbPath = try? FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false).appendingPathComponent("Swift_Console_App/ProjectManager/ProjectManager.sqlite")
+        
+//        print(try? FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false).appendingPathComponent("Swift_Console_App/ProjectManager.sqlite"))
+        
+        guard let dbPath = dbPath else {
+            print("Path is nil")
+            return
+        }
+        
+        //Opening database connectivity
+        if sqlite3_open(dbPath.path, &db) == SQLITE_OK {
+            print("Successfully opened DB at \(dbPath)")
+        } else {
+            print("ERROR : \(String(cString: sqlite3_errmsg(db)))")
+        }
+    }
+}
--- ProjectManager/ProjectManager/DBModels.swift
+++ ProjectManager/ProjectManager/DBModels.swift
@@ -6,3 +6,61 @@
 //
 
 import Foundation
+
+class Project {
+    var batchID: Int?
+    var projectTitle: String
+    var ProjectArea: String
+    var student1: Int
+    var student2: Int
+    var student3: Int
+    var mentorID: Int
+    var progress: Int
+    
+    init(batchID: Int? = nil, projectTitle: String, ProjectArea: String, student1: Int, student2: Int, student3: Int, mentorID: Int, progress: Int) {
+        self.batchID = batchID
+        self.projectTitle = projectTitle
+        self.ProjectArea = ProjectArea
+        self.student1 = student1
+        self.student2 = student2
+        self.student3 = student3
+        self.mentorID = mentorID
+        self.progress = progress
+    }
+}
+
+class Student {
+    var studentId: Int?
+    var studentName: String
+    var dept: String
+    var section: Character
+    var contact: Int
+    var mentorId: Int
+    
+    init(studentId: Int? = nil, studentName: String, dept: String, section: Character, contact: Int, mentorId: Int) {
+        self.studentId = studentId
+        self.studentName = studentName
+        self.dept = dept
+        self.section = section
+        self.contact = contact
+        self.mentorId = mentorId
+    }
+}
+
+class Mentor {
+    var mentorId: Int?
+    var mentorName: String
+    var areaOfSpec: String
+    var dept: String
+    var contact: Int
+    var noOfBatch: Int
+    
+    init(mentorId: Int? = nil, mentorName: String, areaOfSpec: String, dept: String, contact: Int, noOfBatch: Int) {
+        self.mentorId = mentorId
+        self.mentorName = mentorName
+        self.areaOfSpec = areaOfSpec
+        self.dept = dept
+        self.contact = contact
+        self.noOfBatch = noOfBatch
+    }
+}
